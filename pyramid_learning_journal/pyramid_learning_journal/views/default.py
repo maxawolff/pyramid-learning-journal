0751a5d9415e0e27d18671ba085cb2e13a54f4e6
Philip Werner <philip.r.werner@gmail.com>
2017-10-30 15:50:16 -0700

    added scaffold.

---
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -0,0 +1,18 @@
+from pyramid.response import Response
+from pyramid.response import Response
+import OS
+
+
+def list_view(request):
+    """."""
+
+
+def detail_view(request):
+    """."""
+
+
+def create_view(request):
+    """."""
+
+
+def update_view(request):
